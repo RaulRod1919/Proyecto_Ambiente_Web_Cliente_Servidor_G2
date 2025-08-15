8b0c2a37bae000bf9665e2cf8b28390d0907fc66
ElisonHernandez <elisonh158@gmail.com>
2025-08-14 22:40:19 -0600

    falta usuario logi

--- app/config/db.php
+++ app/config/db.php
@@ -2,8 +2,8 @@
 
 
 class Database{
-//Cambiar los datos de conexión a la base de datos de ser necesario
+//Cambiar los datos de conexión a la base de datos de ser necesario joseraulrp196:)
 static function connect(){
-    return $conn = new mysqli("localhost", "root", "joseraulrp196:)", "vecireport");
+    return $conn = new mysqli("localhost", "root", "", "vecireport5");
 }
 }
